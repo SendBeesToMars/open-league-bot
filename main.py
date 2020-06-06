20c41a7eb56e0a734e4d0f48da942e08e27d1953
Winmillion <datum1337@gmail.com>
2020-06-06 18:01:33 +0100

    at max players in group, group gets split randomly and players get pinged

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from discord.ext import commands
 import discord
 import os
+import random
 
 TOKEN = os.environ.get('DOOTDOOT_TOKEN')
+MAX_GROUP_SIZE = 12
 
-players = {}
+players = []
 
-fake_players = {
-    9082734659823745: "player1",
-    9082734659823741: "player2",
-    9082734659823742: "player3",
-    9082734659823743: "player4",
-    9082734659823744: "player5",
-    9082734659823746: "player6",
-    9082734659823711: "player7",
-    9082734659823722: "player8",
-    9082734659823733: "player9",
-    9082734659823784: "player10",
-    9082734659823794: "player11"}
+# fake_players = {
+#     111: "player1",
+#     222: "player2",
+#     333: "player3",
+#     444: "player4",
+#     555: "player5",
+#     666: "player6",
+#     777: "player7",
+#     888: "player8",
+#     999: "player9",
+#     12312312: "player10",
+#     6969696969696969696: "player11"}
+
+fake_players = [111, 222, 333, 444, 555, 666, 777, 888, 999, 123123123, 178178178178]
 
 players = fake_players
 
@@ -34,24 +38,36 @@ async def join(ctx):
     global players
     if ctx.author == bot.user or ctx.channel.name != "bot":
         return
-    if players.get(ctx.author.name) != ctx.author.id:
-        players[ctx.author.id] = ctx.author.name
-    else:
-        players[ctx.author.id] = ctx.author.name + "(1)"
+    
+    if ctx.author.id not in players:
+        players.append(ctx.author.id)
+
     embed = discord.Embed(
-        title=f"Lobby [{len(players)}/12]", 
+        title=f"Lobby [{len(players)}/{MAX_GROUP_SIZE}]", 
         #   prints out name of players joined with index
-        description="\n".join(f"[{i}] " + players[player] for i, player in enumerate(players, start=1)), 
+        description="\n".join(f"[{i}] <@{player}>" for i, player in enumerate(players, start=1)), 
         color=0x00FF00)
     await ctx.send(embed=embed)
-    if len(players) == 12:
-        await ctx.send("omg the partys full :)")
+
+    #   splist group of MAX_GRUUP_SIZE into two, then prints out the two groups
+    if len(players) >= MAX_GROUP_SIZE:
+        await ctx.send("Group 1")
+        randomised_players = random.sample(players, int(MAX_GROUP_SIZE/2))
+        await ctx.send("\n".join(f"<@{player}>" for player in randomised_players))
+        await ctx.send("Group 2")
+        await ctx.send("\n".join(f"<@{player}>" for player in list(set(players) - set(randomised_players))))
+        return
 
 @bot.command(name="q")
 async def quit(ctx):
     if ctx.author == bot.user or ctx.channel.name != "bot":
         return
     await ctx.bot.close()
+
+@bot.command(name="c")
+async def check(ctx):
+    print(ctx.author)
+    await ctx.send(f"<@{ctx.author.id}>")
 
 @bot.command(name="nick")
 async def nick(ctx, arg1):
@@ -63,4 +79,4 @@ bot.run(TOKEN)
 
 # pick random captains
 # pick random teams =p @name
-# print people in queue
+# print people in queue =q
